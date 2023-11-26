5d28a3d2bdc211fed2842a064268f4ff48b872ba
Diego Duarte <duartqx@gmail.com>
2023-11-26 17:47:34 -0300

    Filter documentation, fix and replaced Execute call

--- api/controllers/indexController.go
+++ api/controllers/indexController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 	"strings"
 
@@ -42,17 +43,36 @@ func (ic IndexController) Index(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Filter is a method of the IndexController struct used to filter tasks based on various criteria.
+// This method accepts an HTTP request and an HTTP response writer as arguments.
+// It reads the request body and passes it to the GetTasksByFilter service to retrieve tasks matching the filter criteria.
+// The filter criteria are supplied in the request body in JSON format. The following fields are supported:
+// - "tag": A string specifying the tag of the tasks to retrieve.
+// - "completed": An integer specifying the completion status of the tasks to retrieve. Can be one of the following:
+//   - 0: Ignore the completion status.
+//   - 1: Retrieve only completed tasks.
+//   - 2: Retrieve only not completed tasks.
+//
+// - "from_start_at": A time.Time specifying the earliest start time of the tasks to retrieve.
+// - "to_start_at": A time.Time specifying the latest start time of the tasks to retrieve.
+// - "from_end_at": A time.Time specifying the earliest end time of the tasks to retrieve.
+// - "to_end_at": A time.Time specifying the latest end time of the tasks to retrieve.
+// If an error occurs while decoding the request body, the method responds with a 400 Bad Request status.
+// If any other error occurs while retrieving the tasks, the method panics.
+// If the tasks are retrieved successfully, they are passed to the Execute method of the view along with the response writer to generate the response.
+// If an error occurs while executing the view, the method panics.
 func (ic IndexController) Filter(w http.ResponseWriter, r *http.Request) {
 	tasks, err := ic.service.GetTasksByFilter(r.Body)
 	if err != nil {
-		if strings.HasPrefix("Decode Error:", err.Error()) {
+		if strings.HasPrefix(err.Error(), "Decode Error:") {
+			log.Println(err)
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		} else {
 			panic(err)
 		}
 	}
-	if err := ic.view.Execute(w, tasks); err != nil {
+	if err := ic.view.ExecuteResults(w, tasks); err != nil {
 		panic(err)
 	}
 }
