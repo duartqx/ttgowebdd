4dc888dd1c62203eecb54a8d0056752ffc70af40
Diego Duarte <duartqx@gmail.com>
2023-11-26 02:13:28 -0300

    domains taskRepository interface

---
+++ domains/repositories/taskRepository.go
@@ -0,0 +1,22 @@
+package repositories
+
+import (
+	"io"
+
+	m "github.com/duartqx/ttgowebdd/domains/models"
+)
+
+type TaskRepository interface {
+	Filter(reader io.ReadCloser) (*[]m.Task, error)
+
+	FindById(id int64) (m.Task, error)
+	FindByTag(tag string) (m.Task, error)
+
+	Create(task m.Task) error
+
+	Complete(task m.Task) error
+	CompleteById(id int) (m.Task, error)
+
+	UpdateEndAt(task m.Task) error
+	UpdateEndAtById(id int) (m.Task, error)
+}
