a6ba618fcce7e5f489516d74e37cd952bae9e7c0
Diego Duarte <duartqx@gmail.com>
2023-11-26 02:18:30 -0300

    main

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/duartqx/ttgowebdd
 
 go 1.21.4
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.18 // indirect
+)
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/duartqx/ttgowebdd/api/router"
+	"github.com/duartqx/ttgowebdd/infraestructure/sqlite"
+)
+
+func main() {
+	db, err := sqlite.NewDbConnection("ddclitskt.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	mux := router.NewRouterBuilder().
+		SetDb(db).
+		SetTemplates(&[]string{
+			"./presentation/templates/index.html",
+		}).
+		Build()
+
+	port := ":8000"
+
+	srv := &http.Server{
+		Handler:      mux,
+		Addr:         "127.0.0.1" + port,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
