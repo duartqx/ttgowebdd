793119c73905cfe71fd71d91b3117b6daf7dba02
Diego Duarte <duartqx@gmail.com>
2023-11-26 02:18:03 -0300

    chi router

---
+++ api/router/chi.go
@@ -0,0 +1,62 @@
+package router
+
+import (
+	"html/template"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/jmoiron/sqlx"
+
+	c "github.com/duartqx/ttgowebdd/api/controllers"
+	s "github.com/duartqx/ttgowebdd/application/services"
+	r "github.com/duartqx/ttgowebdd/infraestructure/sqlite/repositories"
+	v "github.com/duartqx/ttgowebdd/presentation/views"
+
+	lm "github.com/duartqx/ttgowebdd/application/middleware/logger"
+	rm "github.com/duartqx/ttgowebdd/application/middleware/recovery"
+)
+
+type router struct {
+	db        *sqlx.DB
+	templates *[]string
+}
+
+func NewRouterBuilder() *router {
+	return &router{
+		templates: &[]string{},
+	}
+}
+
+func (ro *router) SetDb(db *sqlx.DB) *router {
+	ro.db = db
+	return ro
+}
+
+func (ro *router) SetTemplates(templates *[]string) *router {
+	ro.templates = templates
+	return ro
+}
+
+func (ro router) Build() *chi.Mux {
+
+	tmplEngine := template.Must(template.ParseFiles(*ro.templates...))
+	indexView := v.NewIndexView(tmplEngine)
+
+	taskRepository := r.NewTaskRepository(ro.db)
+	taskService := s.NewTaskService(taskRepository)
+
+	taskController := c.NewTaskController(indexView, taskService)
+	indexController := c.NewIndexController(indexView, taskService)
+
+	router := chi.NewRouter()
+
+	router.Use(rm.RecoveryMiddleware, lm.LoggerMiddleware)
+
+	router.Get("/", indexController.Index)
+	router.Post("/", indexController.Filter)
+
+	router.Post("/", taskController.Create)
+	router.Put("/{id:int}", taskController.UpdateEndAt)
+	router.Patch("/{id:int}", taskController.UpdateComplete)
+
+	return router
+}
